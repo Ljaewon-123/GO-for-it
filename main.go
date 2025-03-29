28006b9d3ae2945e4c2718fa361554ccae3b0a7e
Ljaewon-123 <jml6388@naver.com>
2025-03-29 16:40:38 +0900

    변수선언

--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 // execute ## go run main.go
 package main // 패키지 이름 , main.go 컴파일 원하면 필요함
-import (
-	"fmt"
-
-	"github.com/Ljaewon-123/GO-for-it/something"
-)
+import "fmt"
 
 func main() {
-	fmt.Println("Hello world!")
-	something.SayHello()
+	// const name string = "jaewon"
+	// name = "Suz" can't change
 
+	// 축약코드 사용시 Go가 타입을 찾아줌
+	name := "jaewon" // == var name string = "jaewon"
+	name = "하이"      // can change
+	fmt.Println(name)
 }
