07770f3a2f884e662a4020ceffe84723c93abbcf
Ljaewon-123 <jml6388@naver.com>
2025-03-29 17:03:13 +0900

    go func

--- main.go
+++ main.go
@@ -1,13 +1,34 @@
 // execute ## go run main.go
 package main // 패키지 이름 , main.go 컴파일 원하면 필요함
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+//	func multiply(a int, b int) int {
+//		return a * b
+//	}
+func multiply(a, b int) int {
+	return a * b
+}
+
+// multi return function
+func lanAndUpper(name string) (int, string) {
+	return len(name), strings.ToUpper(name)
+}
+
+func repeatMe(words ...string) {
+	fmt.Println(words)
+}
 
 func main() {
-	// const name string = "jaewon"
-	// name = "Suz" can't change
+	fmt.Println(multiply(2, 2))
 
-	// 축약코드 사용시 Go가 타입을 찾아줌
-	name := "jaewon" // == var name string = "jaewon"
-	name = "하이"      // can change
-	fmt.Println(name)
+	totalLen, upperName := lanAndUpper("jaewon")
+	fmt.Println(totalLen, upperName)
+
+	totalLen2, _ := lanAndUpper("jaewon")
+	fmt.Println(totalLen2)
+
+	repeatMe("jaewon", "suz", "dal", "marl")
 }
